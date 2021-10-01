ead1b0a8351c9df6cb70c2b181fc7f355034d3bc
Пигасов Фёдор <90041696+Doderok@users.noreply.github.com>
2021-10-01 16:12:21 +0300

    Add files via upload

---
+++ pyton_3.py
@@ -0,0 +1,11 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+a=float(input("Введите n:"))
+b=((1+a)*a/2)
+print("сумма чисел от 1 до n",b)
