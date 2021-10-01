decb6866649323a82252f7850b7d374b7823fb72
Пигасов Фёдор <90041696+Doderok@users.noreply.github.com>
2021-10-01 22:37:46 +0300

    Add files via upload

---
+++ pyton_4.py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+a=float(input("введите часы:"))
+b=float(input("введите минуты:"))
+c=float(input("введите cекунды:"))
+t=a*3600+b*60+c
+print("сумарное число секунд",t)
