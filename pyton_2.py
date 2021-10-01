ab04a969e15c845171144e338dbc12a191e5c7e0
Пигасов Фёдор <90041696+Doderok@users.noreply.github.com>
2021-10-01 16:10:40 +0300

    Add files via upload

---
+++ pyton_2.py
@@ -0,0 +1,12 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+a= float(input("Введите длинну:"))
+b= float(input("Введите ширину:"))
+c=a*b
+print("Площадь комнаты",c)
